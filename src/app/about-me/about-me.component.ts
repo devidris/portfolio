00aae3e0ed1cf146b9c79b64d3436eddc00c86d0
oppourtunity <info@dev-idris.com>
2022-07-10 16:47:58 +0000

    hover animation to about me imagest

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -1,15 +1,65 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  state,
+  style,
+  transition,
+  trigger,
+  animate,
+  keyframes,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-about-me',
   templateUrl: './about-me.component.html',
-  styleUrls: ['./about-me.component.scss']
+  styleUrls: ['./about-me.component.scss'],
+  animations: [
+    trigger('move', [
+      state(
+        'start',
+        style({
+          transform: 'translate({{xi}}%,{{yi}}%)',
+        }),
+        { params: { xi: 0, yi: 0 } }
+      ),
+      state(
+        'end',
+        style({
+          transform: 'translate({{xi}}%,{{yi}}%)',
+        }),
+        { params: { xi: 0, yi: 0 } }
+      ),
+      transition('end<=>start', [animate('500ms')]),
+    ]),
+  ],
 })
 export class AboutMeComponent implements OnInit {
+  constructor() {}
+  x: number = 0;
+  y: number = 0;
+  i: number = 0;
+  xi: number = 0;
+  yi: number = 0;
+  move: string = '';
 
-  constructor() { }
+  // Generates numbers depending on how you move your mouse
+  hovered(e: any) {
+    if (this.x > e.screenX) this.xi += 2;
+    if (this.x < e.screenX) this.xi -= 2;
+    if (this.y > e.screenY) this.yi += 2;
+    if (this.y < e.screenY) this.yi -= 2;
 
-  ngOnInit(): void {
+    this.x = e.screenX;
+    this.y = e.screenY;
+
+    this.move = 'start';
   }
 
+  // Reset the xi and yi
+  unHovered() {
+    this.xi = 0;
+    this.yi = 0;
+    this.move = 'end';
+  }
+
+  ngOnInit(): void {}
 }
